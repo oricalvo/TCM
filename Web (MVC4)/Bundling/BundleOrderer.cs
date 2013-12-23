cc00317260244285cfaab8df70e61ff7f0bce800
oric <ori.calvo@gmail.com>
2013-12-23 22:04:11 +0200

    First commit

---
+++ Web (MVC4)/Bundling/BundleOrderer.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Optimization;
+
+namespace TCM.Web.Bundling.Core
+{
+    public class BundleOrderer : IBundleOrderer
+    {
+        private int maxLinesToParseForScriptReferences;
+
+        public BundleOrderer(int maxLinesToParseForScriptReferences = 25)
+        {
+            this.maxLinesToParseForScriptReferences = maxLinesToParseForScriptReferences;
+        }
+
+        private class Node
+        {
+            public string ID;
+            public List<Node> Children;
+            public FileInfo FileInfo;
+            public bool IsRoot;
+            public int Level;
+
+            public Node(string id)
+            {
+                this.ID = id;
+                this.Children = new List<Node>();
+                this.IsRoot = true;
+                this.Level = 0;
+            }
+        }
+
+        public IEnumerable<FileInfo> OrderFiles(BundleContext context, IEnumerable<FileInfo> filesIn)
+        {
+            FileInfo[] files = filesIn.ToArray();
+            Dictionary<string, Node> nodes = new Dictionary<string, Node>();
+
+            //
+            //  Build a node and collect dependency information for each file
+            //
+            foreach (FileInfo file in files)
+            {
+                //
+                //  Create a node for the current file (the node may be already created)
+                //
+                string fileId = GetFileID(file.FullName);
+                Node node = GetCreateNode(nodes, fileId);
+                node.FileInfo = file;
+
+                //
+                //  Extract dependencies from script header
+                //
+                string[] dependencies = ParseDepenedencies(file);
+                foreach (string dependencyId in dependencies)
+                {
+                    Node dependencyNode = GetCreateNode(nodes, dependencyId);
+                    dependencyNode.Children.Add(node);
+                }
+
+                if (dependencies.Length > 0)
+                {
+                    //
+                    //  Since this node has dependency it is no longer considered a root
+                    //
+                    node.IsRoot = false;
+                }
+            }
+
+            //
+            //  Orgnaize nodes by level. Level 0 is a root node which has no dependency.
+            //
+            foreach (Node rootNode in (from n in nodes.Values where n.IsRoot select n))
+            {
+                WalkBFS(rootNode, 0);
+            }
+
+            Dictionary<int, List<Node>> nodesByLevel = new Dictionary<int, List<Node>>();
+            foreach (Node node in nodes.Values)
+            {
+                List<Node> levelNodes;
+                if (!nodesByLevel.TryGetValue(node.Level, out levelNodes))
+                {
+                    levelNodes = new List<Node>();
+                    nodesByLevel.Add(node.Level, levelNodes);
+                }
+
+                levelNodes.Add(node);
+            }
+
+            //
+            //  Merge all nodes into flat list. First are level 0 and then level 1 and so on
+            //
+            List<Node> flat = new List<Node>();
+            int level = 0;
+            while (nodesByLevel.ContainsKey(level))
+            {
+                flat.AddRange(nodesByLevel[level]);
+
+                ++level;
+            }
+
+            //
+            //  res now contains the final flat list organized in the right order
+            //
+            FileInfo[] res = (from node in flat where node.FileInfo != null select node.FileInfo).ToArray();
+            return res;
+        }
+
+        private string[] ParseDepenedencies(FileInfo file)
+        {
+            List<string> dependencies = new List<string>();
+
+            using (StreamReader reader = file.OpenText())
+            {
+                int lineCount = 0;
+
+                while (true)
+                {
+                    if (lineCount > this.maxLinesToParseForScriptReferences)
+                    {
+                        //
+                        //  Only scan the first 100 lines
+                        //  js file generated from ts contains generated method like __extends.
+                        //  Therefore we cannot assume that script reference are always at the top of the file
+                        //
+                        break;
+                    }
+
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        //
+                        //  End of file
+                        //
+                        break;
+                    }
+                    ++lineCount;
+
+                    line = line.Trim();
+
+                    if (!line.StartsWith("/// <reference"))
+                    {
+                        continue;
+                    }
+
+                    int begin = line.IndexOf('"');
+                    int end = line.IndexOf('"', begin + 1);
+                    if (begin == -1 || end == -1)
+                    {
+                        //
+                        //  Bad reference syntax. Ignore this line
+                        //
+                        continue;
+                    }
+
+                    string dependencyRelativePath = line.Substring(begin + 1, end - begin - 1);
+                    string dependencyFullPath = GetFilePath(Path.GetFullPath(Path.Combine(file.DirectoryName, dependencyRelativePath)));
+
+                    if (!File.Exists(dependencyFullPath))
+                    {
+                        throw new Exception("Script " + file.Name + " contains invalid script reference " + dependencyRelativePath);
+                    }
+
+                    string dependencyFileName = Path.GetFileName(dependencyFullPath);
+                    string dependencyId = GetFileID(dependencyFullPath);
+
+                    dependencies.Add(dependencyId);
+                }
+            }
+
+            return dependencies.ToArray();
+        }
+
+        private Node GetCreateNode(Dictionary<string, Node> nodes, string fileId)
+        {
+            Node node;
+
+            if (!nodes.TryGetValue(fileId, out node))
+            {
+                node = new Node(fileId);
+                nodes.Add(fileId, node);
+            }
+
+            return node;
+        }
+
+        private string GetFilePath(string filePath)
+        {
+            string res = filePath.Replace(".ts", ".js");
+            return res;
+        }
+
+        private string GetFileID(string filePath)
+        {
+            string res = filePath.ToLower().Replace(".ts", ".js");
+            return res;
+        }
+
+        private void WalkBFS(Node root, int level)
+        {
+            if (root.Level < level)
+            {
+                //
+                //  Only update if level is higher than calculated up until now
+                //
+                root.Level = level;
+            }
+
+            foreach (Node child in root.Children)
+            {
+                WalkBFS(child, level + 1);
+            }
+        }
+    }
+}
---
+++ Web (MVC4)/Bundling/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Optimization;
+
+namespace TCM.Web.Bundling.Core
+{
+    public static class BundleCollectionExtensions
+    {
+        private static IBundleOrderer defaultOrderer;
+
+        static BundleCollectionExtensions()
+        {
+            defaultOrderer = new BundleOrderer();
+        }
+
+        public static void IncludeDirectory(this BundleCollection bundles, string bundleVirtualPath, params string[] directories)
+        {
+            ScriptBundle bundle = new ScriptBundle(bundleVirtualPath);
+            foreach (string dir in directories)
+            {
+                DoIncludeDirectory(bundle, dir);
+            }
+
+            bundle.Orderer = defaultOrderer;
+            bundles.Add(bundle);
+        }
+
+        private static void DoIncludeDirectory(ScriptBundle bundle, string virtualDir)
+        {
+            string dirPath = HttpContext.Current.Server.MapPath(virtualDir);
+            foreach (string filePath in Directory.GetFiles(dirPath, "*.js"))
+            {
+                if (ExcludeScript(filePath))
+                {
+                    continue;
+                }
+
+                bundle.Include(virtualDir + "/" + Path.GetFileName(filePath));
+            }
+
+            foreach (string subDirPath in Directory.GetDirectories(dirPath))
+            {
+                string dirName = Path.GetFileName(subDirPath);
+                string virtualSubDir = virtualDir + "/" + dirName;
+
+                DoIncludeDirectory(bundle, virtualSubDir);
+            }
+        }
+
+        private static bool ExcludeScript(string filePath)
+        {
+            string fileName = Path.GetFileName(filePath);
+            if (string.Compare(fileName, "_references.js", true) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void SetDefaultBundleOrderer(this BundleCollection bundles, IBundleOrderer defaultOrderer)
+        {
+            BundleCollectionExtensions.defaultOrderer = defaultOrderer;
+        }
+    }
+}
